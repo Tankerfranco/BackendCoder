2f3bc7acaca201c0b1cec159f4a78f08c5e54e12
Franco Negrete <franconegrete12@gmail.com>
2022-05-12 18:16:52 -0300

    Desafio 3

---
+++ clase6/desafio.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const contenedor = require('./desafioPasado');
+
+function getRandom () {
+    return Math.round(Math.random()*3)
+}
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+})
+
+
+app.get('/productos', (req, res) => {
+    const ejecutar = async () => {
+        const productos = new contenedor('producto.txt');
+        const contenido = await productos.getAll();
+        res.send(contenido);
+    }
+    ejecutar();
+})
+
+app.get('/productoRandom', (req, res) => {
+    const ejecutar = async () => {
+        const productos = new contenedor('producto.txt');
+        const contenido = await productos.getById(getRandom());
+        res.send(contenido);
+    }
+    ejecutar();
+})
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado en el puerto 8080');
+})
---
+++ clase6/desafioPasado.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+class Contenedor{
+    id = 1;
+    constructor(nombreArchivo){
+        this.nombreArchivo = nombreArchivo;
+        
+    }
+
+    async getAll(){
+        try{
+            const contenidoCrudo = await fs.promises.readFile(this.nombreArchivo, 'utf-8');
+            const contenido = JSON.parse(contenidoCrudo);
+            return contenido;
+        }catch(error){  
+            console.log(error);
+            return [];
+        }
+    }
+
+    async save(objeto){
+        objeto['id'] = this.id;
+        this.id++;
+        const contenido = JSON.parse(await fs.promises.readFile(this.nombreArchivo, 'utf-8'));
+        contenido.push(objeto);
+        await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(contenido));
+    }
+
+    async getById(id){
+        const contenido = await this.getAll();
+        const objeto = contenido.find(obj => obj.id === id);
+        return objeto;
+    }
+
+    async deleteAll(){
+        await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([]));
+    }
+
+    async deletebyId(id){
+        const contenido = await this.getAll();
+        const objeto = contenido.find(obj => obj.id === id);
+        const index = contenido.indexOf(objeto);
+        contenido.splice(index, 1);
+        await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(contenido));
+    }
+}
+
+
+const ejecutarProductos = async () => {
+    const productos = new Contenedor('producto.txt');
+    await productos.save({title: 'Coca Cola', price: 50.50, thumbnail: 'coca-cola.jpg'});
+    await productos.save({title: 'Fanta', price: 50.50, thumbnail: 'fanta.jpg'});
+    await productos.save({title: 'Sprite', price: 50.50, thumbnail: 'sprite.jpg'});
+}
+
+
+ejecutarProductos();
+
+module.exports = Contenedor;
